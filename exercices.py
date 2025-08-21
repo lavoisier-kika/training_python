6eba635e0adcbfdfb6d66e1e2cafd13b3034d0e3
lavoisier-kika <lavoisiermuibombe@gmail.com>
2025-08-21 16:46:28 +0200

    ajout du fichier exercixe.py

--- exercices.py
+++ exercices.py
@@ -1,25 +1,9 @@
-""" 
-a = False
-print(a)
-print(type(a))
+kika = input("bonjour")
+print("Bonjour " + kika)
 
-b= 4
-print(b)
-print(type(b))
-
-c= 10.3
-print(c)
-print(type(c))
-
-d = 3+4j
-print(d)
-print(type(d))
-
-e="Hello"
-print(e)
-print(type(e))
+"""
+print("Programming","Essentials","in", sep="***", end="..."  )
+print("Python")
 """
 
-nom = input("Quel est votre nom ? ")
-couleur = input("Quel est votre couleur préferée ? ")
-print("Bonjour " + nom + " votre couleur préférée est " + couleur )
+
